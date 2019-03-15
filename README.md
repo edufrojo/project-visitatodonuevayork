328e8e68d4b1081f0effcdd010e9d14ec760c83a
edufrojo <edufrojo@gmail.com>
2019-03-15 10:15:26 +0100

    fix(general): corregir mail

---
+++ README.md
@@ -0,0 +1,15 @@
+# project-visitatodonuevayork
+
+Project "visitatodonuevayork.es"
+
+[![Build Status](https://travis-ci.org/edufrojo/project-visitatodonuevayork.svg?branch=master)](https://travis-ci.org/edufrojo/project-visitatodonuevayork)
+[![dependencies Status](https://david-dm.org/edufrojo/project-visitatodonuevayork/status.svg)](https://david-dm.org/edufrojo/project-visitatodonuevayork)
+[![devDependencies Status](https://david-dm.org/edufrojo/project-visitatodonuevayork/dev-status.svg)](https://david-dm.org/edufrojo/project-visitatodonuevayork?type=dev)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/edufrojo/project-visitatodonuevayork/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/edufrojo/project-visitatodonuevayork/?branch=master)
+[![HitCount](http://hits.dwyl.io/edufrojo/project-visitatodonuevayork.svg)](http://hits.dwyl.io/edufrojo/project-visitatodonuevayork)
+
+## Licencia
+
+[![GitHub license](https://img.shields.io/github/license/edufrojo/project-visitatodonuevayork.svg)](https://github.com/edufrojo/project-visitatodonuevayork/blob/master/LICENSE)
+
+En la medida de lo posible según la ley, [Eduardo Fernández](https://edufrojo.dev) ha renunciado a todos los derechos de autor y derechos conexos o relacionados a este trabajo.
--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const mail = require('nodemailer').mail;
+const sendmail = require('sendmail');
 
 // Creamos servidor y configuramos el puerto
 const app = express();
@@ -34,35 +34,52 @@ app.use('/assets', [
   express.static(__dirname + '/node_modules/normalize.css/'),
   express.static(__dirname + '/node_modules/animate.css/'),
   express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/'),
-  express.static(__dirname + '/node_modules/cookieconsent/build/')
+  express.static(__dirname + '/node_modules/cookieconsent/build/'),
 ]);
 
 app.use(cookieParser('keyboard cat'));
-app.use(session({ resave: true, saveUninitialized: true, secret: 'keyboard cat', cookie: { maxAge: 60000 } }));
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'keyboard cat',
+    cookie: { maxAge: 60000 },
+  }),
+);
+
+// Mail Configuration
+app.post('/enviar-mensaje', function(req, res) {
+  let mailFrom = req.body.txtMail;
+  let mailTo = 'edufrojo@gmail.com';
+  let mailSubject = 'Mensaje de ' + req.body.txtNombre + ' desde visitatodonuevayork.es';
+  let mailHtml =
+    '<p><h4>Nombre:</h4>' +
+    req.body.txtNombre +
+    '</p>' +
+    '<p><h4>Correo Electrónico</h4> ' +
+    req.body.txtMail +
+    '</p>' +
+    '</p><h4>Mensaje</h4> ' +
+    req.body.txtMensaje +
+    '</p>';
+
+  sendmail(
+    {
+      from: mailFrom,
+      to: mailTo,
+      subject: mailSubject, // Subject line
+      html: mailHtml,
+      attachments: [],
+    },
+    function(err, reply) {
+      console.log(err && err.stack);
+      console.dir(reply);
+    },
+  );
+});
 
 // Habilitamos servidor
 const server = http.createServer(app);
 server.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
-});
-
-// Mail Configuration
-app.post('/enviar-mensaje', function(req, res) {
-  mail({
-    from: req.body.txtMail,
-    to: 'visitatodonuevayork@gmail.com',
-    subject: 'Mensaje de ' + req.body.txtNombrels + ' desde visitatodonuevayork.es',
-    html:
-      '<p><h4>Nombre:</h4> ' +
-      req.body.txtNombre +
-      '</p>' +
-      '<p><h4>Correo Electrónico</h4> ' +
-      req.body.txtMail +
-      '</p>' +
-      '</p><h4>Mensaje</h4> ' +
-      req.body.txtMensaje +
-      '</p>'
-  });
-
-  res.redirect('/');
 });
